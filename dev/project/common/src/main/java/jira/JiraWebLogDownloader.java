214a844424c9d76a9607b809890b17cf25aec972
Marcin Wolak <mtwolak@gmail.com>
2016-10-31 18:42:54 +0100

    Added method to jira interface

--- dev/project/common/src/main/java/jira/JiraWebLogDownloader.java
+++ dev/project/common/src/main/java/jira/JiraWebLogDownloader.java
@@ -1,5 +1,11 @@
 package jira;
 
+import java.net.URL;
+import java.util.List;
+
+import jira.issue.JiraIssue;
+
 public interface JiraWebLogDownloader {
+	public List<JiraIssue> retrieveAllIssues(URL jiraWebUrl);
 
 }
