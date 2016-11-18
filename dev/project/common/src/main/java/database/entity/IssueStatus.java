d52de23886ae98817f050c42f080d071d4eb84aa
Marcin Wolak <mtwolak@gmail.com>
2016-11-18 22:02:38 +0100

    Removed issue status table, converted to enum

--- dev/project/common/src/main/java/database/entity/IssueStatus.java
+++ dev/project/common/src/main/java/database/entity/IssueStatus.java
@@ -1,48 +1,7 @@
 package database.entity;
 
-import java.util.Set;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-
-@Entity
-@Table(name = "ISSUE_STATUS")
-public class IssueStatus {
-
-	@Id
-	@Column(name = "ISSUE_STATUS_ID", nullable = false)
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private int id;
-
-	@Column(name = "STATUS_NAME", nullable = false, unique = true)
-	private String statusName;
-
-	@OneToMany(targetEntity = JiraIssue.class, mappedBy = "issueStatus")
-	private Set<JiraIssue> jiraIssues;
-
-	public String getStatusName() {
-		return statusName;
-	}
-
-	public void setStatusName(String statusName) {
-		this.statusName = statusName;
-	}
-
-	public Set<JiraIssue> getJiraIssues() {
-		return jiraIssues;
-	}
-
-	public void setJiraIssues(Set<JiraIssue> jiraIssues) {
-		this.jiraIssues = jiraIssues;
-	}
-
-	public int getId() {
-		return id;
-	}
+public enum IssueStatus {
+	RESOLVED, CLOSED
 
 }
