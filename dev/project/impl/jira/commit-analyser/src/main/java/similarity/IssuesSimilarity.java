eb5485a3c531733efbac31d280c69e54d91bcbc6
Tomasz Wasielewski <tomek.wasiel@gmail.com>
2016-11-20 02:38:04 +0100

    Issues descriptions similarity using lucene

--- dev/project/impl/jira/commit-analyser/src/main/java/similarity/IssuesSimilarity.java
+++ dev/project/impl/jira/commit-analyser/src/main/java/similarity/IssuesSimilarity.java
@@ -1,9 +1,15 @@
 package similarity;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
+import org.hibernate.collection.internal.PersistentSet;
+import org.hibernate.mapping.Set;
+
 import database.application.DatabaseApplication;
+import database.entity.IssueComment;
 import database.entity.JiraIssue;
 import database.entity.JiraProject;
 import javafx.util.Pair;
@@ -11,21 +17,19 @@ import lucene.CosineTextSimilarity;
 
 public class IssuesSimilarity 
 {
-	static DatabaseApplication dba = new DatabaseApplication();
-	static List<Pair<Integer, Double>> similarities;
-	CosineTextSimilarity cts;
+	DatabaseApplication dba;
 	
-	public static void main(String[] args)
-	{
-		JiraIssue issue = dba.getJiraIssue(1);
-		System.out.println(issue);
-		getIssueSimilarityList(issue);
-	}
-	
-	public static ArrayList<Pair<Integer, Double>> getIssueSimilarityList(JiraIssue issue)
+	public IssuesSimilarity(JiraIssue issue)
 	{
 		dba = new DatabaseApplication();
-		similarities = new ArrayList<Pair<Integer, Double>>();
+		/*issue = dba.getJiraIssue(1);*/
+		List<Pair<Integer, Double>> similarityList = getIssueSimilarityList(issue);
+	}
+	
+	public List<Pair<Integer, Double>> getIssueSimilarityList(JiraIssue issue)
+	{
+		dba = new DatabaseApplication();
+		List<Pair<Integer, Double>> similarities = new ArrayList<Pair<Integer, Double>>();
 		JiraProject project = issue.getJiraProject();
 		List<JiraIssue> issues = dba.getJiraIssues(project);
 		for(JiraIssue jissue : issues)
@@ -33,13 +37,48 @@ public class IssuesSimilarity
 			similarities.add(new Pair<Integer, Double>(jissue.getJiraIssueId(), getIssueSimilarity(issue, jissue)));
         }
 		dba.closeSession();
+		/*
+		for(Pair<Integer, Double> sim : similarities)
+		{
+			System.out.println(sim);
+		}
+		*/
 		
-		return null;
+		return similarities;
 	}	
 	
-	public static double getIssueSimilarity(JiraIssue issue1, JiraIssue issue2)
+	public double getIssueSimilarity(JiraIssue issue1, JiraIssue issue2)
 	{
-		
+		try 
+		{
+			/*System.out.println("===========================================");
+			PersistentSet comments1 = (PersistentSet) issue1.getIssueComments();
+			Iterator<IssueComment> iterator = comments1.iterator();
+		    while(iterator.hasNext()) {
+		        IssueComment setElement = iterator.next();
+		        if(setElement==null) {
+		            iterator.remove();
+		        }
+		        else
+		        {
+		        	System.out.println(setElement.getContent());
+		        }
+		    }*/
+			
+			String description1 = issue1.getDescription();
+			String description2 = issue2.getDescription();
+			double descriptionSimilarity = 0;
+			if(description1 != null && description2 != null)
+			{
+			   CosineTextSimilarity cts1 = new CosineTextSimilarity(description1, description2);
+			   descriptionSimilarity = cts1.getCosineSimilarity(description1, description2);
+			   return descriptionSimilarity;
+		    }
+		    
+		} catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
 		return -1.0;
 	}
 }
