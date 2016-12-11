560f9aca301e4cbccbd973f5ba09ab6b6b06ac72
Marcin Wolak <mtwolak@gmail.com>
2016-12-11 15:56:13 +0100

    Reject null issue description

--- dev/project/impl/jira/issues-filter/src/main/java/filter/MinimumIssueDescripionSize.java
+++ dev/project/impl/jira/issues-filter/src/main/java/filter/MinimumIssueDescripionSize.java
@@ -15,13 +15,27 @@ public class MinimumIssueDescripionSize implements JiraIssueFilter
 	public boolean filter(AssigneeIssues assigneeWithHisIssues, PropertiesReader propertiesReader)
 	{
 		List<JiraIssue> assignedJiraIssues = new ArrayList<>();
-		for(JiraIssue jiraIssue : assigneeWithHisIssues.getAssignedJiraIssues()) {
-			if(jiraIssue.getDescription().length() >= propertiesReader.getAsInt(Property.FILTER_MIN_DESCRIPTION_LONG_SIZE)) {
-				assignedJiraIssues.add(jiraIssue);
-			}
+		for (JiraIssue jiraIssue : assigneeWithHisIssues.getAssignedJiraIssues())
+		{
+			addJiraIssueWithCorrectDescriptionLength(propertiesReader, assignedJiraIssues, jiraIssue);
 		}
 		assigneeWithHisIssues.setAssignedJiraIssues(assignedJiraIssues);
 		return true;
+	}
+
+	private void addJiraIssueWithCorrectDescriptionLength(PropertiesReader propertiesReader, List<JiraIssue> assignedJiraIssues,
+			JiraIssue jiraIssue)
+	{
+		String issueDescription = jiraIssue.getDescription();
+		if (issueDescription != null && isJiraDescriptionLengthCorrect(propertiesReader, issueDescription))
+		{
+			assignedJiraIssues.add(jiraIssue);
+		}
+	}
+
+	private boolean isJiraDescriptionLengthCorrect(PropertiesReader propertiesReader, String issueDescription)
+	{
+		return issueDescription.length() >= propertiesReader.getAsInt(Property.FILTER_MIN_DESCRIPTION_LONG_SIZE);
 	}
 
 }
