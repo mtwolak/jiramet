660d4a2b40b346da76e30124f1b545a720e88df6
Marcin Wolak <mtwolak@gmail.com>
2016-12-14 20:42:16 +0100

    Added showing no data to predict when time equals 0

--- dev/project/impl/prediction/printer/src/main/java/printer/PredictionTextComposer.java
+++ dev/project/impl/prediction/printer/src/main/java/printer/PredictionTextComposer.java
@@ -14,6 +14,7 @@ public class PredictionTextComposer extends PredictionPrintable
 
 	private static final String NEW_LINE = "\n";
 	private static final int DEFAULT_CAPACITY_STRINGBUILDER_PER_ASSIGNEE = 200;
+	private static final String NOT_ENOUGH_DATA_TO_SHOW = "not enough data to predict";
 
 	@Override
 	protected String getPrediction(JiraIssue newIssue, AssigneeTimeResolve assigneeIssueTimes)
@@ -24,9 +25,14 @@ public class PredictionTextComposer extends PredictionPrintable
 	private String getInformationAboutAssigneeAndTheirTime(AssigneeTimeResolve assigneeIssueTime)
 	{
 		StringBuilder sb = new StringBuilder(DEFAULT_CAPACITY_STRINGBUILDER_PER_ASSIGNEE);
-		sb.append("Assignee: ").append(assigneeIssueTime.getAssignee()).append(", time: ").append(assigneeIssueTime.getPredictedTime())
+		sb.append("Assignee: ").append(assigneeIssueTime.getAssignee()).append(", time: ").append(getTimePrediction(assigneeIssueTime))
 				.append(NEW_LINE);
 		return sb.toString();
+	}
+
+	private String getTimePrediction(AssigneeTimeResolve assigneeIssueTime)
+	{
+		return assigneeIssueTime.getPredictedTime() == 0 ? NOT_ENOUGH_DATA_TO_SHOW : String.valueOf(assigneeIssueTime.getPredictedTime());
 	}
 
 }
