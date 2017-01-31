e2fc3105166ede84ac88de033802618bb41b0f49
Marcin Wolak <mtwolak@gmail.com>
2017-01-31 15:39:58 +0100

    Restored old way of getting percentage of project

--- dev/project/impl/jira/issues-filter/src/main/java/filter/custom/AnalyzedIssueFilter.java
+++ dev/project/impl/jira/issues-filter/src/main/java/filter/custom/AnalyzedIssueFilter.java
@@ -6,6 +6,7 @@ import java.util.List;
 import database.entity.JiraIssue;
 import jira.AssigneeIssues;
 import utils.properties.PropertiesReader;
+
 /**
  * Class for discarding issue which is being analyzed
  */
@@ -13,28 +14,29 @@ public class AnalyzedIssueFilter implements JiraIssueFilter
 {
 
 	JiraIssue analyzedIssue;
-	
+
 	public AnalyzedIssueFilter(JiraIssue analyzedIssue)
 	{
-		this.analyzedIssue = analyzedIssue;	
+		this.analyzedIssue = analyzedIssue;
 	}
 
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
-	public boolean filter(AssigneeIssues assigneeWithHisIssues, PropertiesReader propertiesReader) {
+	public boolean filter(AssigneeIssues assigneeWithHisIssues, PropertiesReader propertiesReader)
+	{
 		List<JiraIssue> assignedJiraIssues = new ArrayList<>();
 		for (JiraIssue jiraIssue : assigneeWithHisIssues.getAssignedJiraIssues())
 		{
-			addJiraIssueThatIsNotTheSameAsAnalyzedOne(propertiesReader, assignedJiraIssues, jiraIssue, analyzedIssue);
+			addJiraIssueThatIsNotTheSameAsAnalyzedOne(propertiesReader, assignedJiraIssues, jiraIssue);
 		}
 		assigneeWithHisIssues.setAssignedJiraIssues(assignedJiraIssues);
 		return true;
 	}
-	
+
 	private void addJiraIssueThatIsNotTheSameAsAnalyzedOne(PropertiesReader propertiesReader, List<JiraIssue> assignedJiraIssues,
-			JiraIssue jiraIssue, JiraIssue analyzedIssue)
+			JiraIssue jiraIssue)
 	{
 		if (jiraIssue.getJiraIssueId() != analyzedIssue.getJiraIssueId())
 		{
